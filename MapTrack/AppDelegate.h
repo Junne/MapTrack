a0197b80f35390fe034d88e4cbae8208a9aa2f62
Junne <junne.bai@gmail.com>
2015-08-20 16:54:51 +0800

    Initial Commit

---
+++ MapTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapTrack
+//
+//  Created by baijf on 8/20/15.
+//  Copyright (c) 2015 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapTrack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapTrack
+//
+//  Created by baijf on 8/20/15.
+//  Copyright (c) 2015 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
